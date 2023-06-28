c8ca3e92d897fff61b84add2e4ed7287ab4a07d2
Sebastian Tilsch <sebastian.tilsch@slub-dresden.de>
2023-06-28 14:43:54 +0200

    add basic test cases

--- test/jsonSchemaToGraphQLQuery.test.ts
+++ test/jsonSchemaToGraphQLQuery.test.ts
@@ -7,21 +7,12 @@ const schema: JSONSchema7 = {
   'title': 'Person',
   'description': 'A human being',
   'type': 'object',
-  'required': ['name', 'father'],
+  'required': ['name', 'likesProduct'],
   'properties': {
     'name': {
       'type': 'string'
     },
-    'knows': {
-      'type': 'array',
-      'items': {
-        'required': ['nick'],
-        'properties': {
-          'nick': {'type': 'string'},
-        }
-      }
-    },
-    'father': {
+    'likesProduct': {
       'type': 'object',
       'properties': {
         'name': {'type': 'string'},
@@ -30,19 +21,43 @@ const schema: JSONSchema7 = {
     }
   }
 }
+
+const schemaWithNestedObjectStub = {
+  '$schema': 'http://json-schema.org/draft-07/schema#',
+  '$id': 'https://example.com/person.schema.json',
+  '$defs': {
+    'person': {
+      'type': 'object',
+      'title': 'Person',
+      'description': 'A human being',
+      'required': ['name', 'knows'],
+      'properties': {
+        'name': {
+          'type': 'string'
+        },
+        'knows': {
+          'type': 'array',
+          'items': {
+            '$ref': '#/$defs/person'
+          }
+        }
+      }
+    }
+  }
+}
+
+const schemaWithNestedObject = {
+  ...schemaWithNestedObjectStub,
+  ...schemaWithNestedObjectStub.$defs.person
+} as JSONSchema7
 describe('graphql list queries', () => {
   it('can be generated out of basic json schema', () => {
     const graphqlQuery = jsonSchemaToGraphQLQuery('Person', schema)
     expect(graphqlQuery).toEqual(
-
-`query getPerson( $pk: ID! ) {
+        `query getPerson( $pk: ID! ) {
       getPerson(pk: $pk) {
       	name
-	knows { 
-	    	nick
-		id 
-	  }
-	father { 
+	likesProduct { 
 	    	name
 		description
 		id 
@@ -51,5 +66,41 @@ describe('graphql list queries', () => {
       }
     }`);
 
+  });
+  it('can be generated out of schema with nested object', () => {
+    const graphqlQuery = jsonSchemaToGraphQLQuery('Person', schemaWithNestedObject)
+    expect(graphqlQuery).toEqual(
+        `query getPerson( $pk: ID! ) {
+      getPerson(pk: $pk) {
+      	name
+	knows { 
+	    	name
+		id 
+	  }
+	id
+      }
+    }`);
+  });
+  it('can be generated out ofschema with nested object and maxRecursion 3', () => {
+    const graphqlQuery = jsonSchemaToGraphQLQuery('Person', schemaWithNestedObject, { maxRecursion: 3})
+    expect(graphqlQuery).toEqual(
+        `query getPerson( $pk: ID! ) {
+      getPerson(pk: $pk) {
+      	name
+	knows { 
+	    	name
+		knows { 
+		    	name
+			knows { 
+			    	name
+				id 
+			  }
+			id 
+		  }
+		id 
+	  }
+	id
+      }
+    }`);
   });
 });
